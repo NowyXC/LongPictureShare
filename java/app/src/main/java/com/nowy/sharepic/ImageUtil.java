f11e6b0676722ffbaa1b8351830411bdfcbf8f3c
nowy <wuliuxin@qtone.cn>
2021-05-27 17:40:01 +0800

    init

---
+++ java/app/src/main/java/com/nowy/sharepic/ImageUtil.java
@@ -0,0 +1,14 @@
+package com.nowy.sharepic;
+
+/**
+ * @ClassName: ImageUtil
+ * @Description: 作用描述
+ * @Author: Nowy
+ * @CreateDate: 2021/5/27 16:02
+ * @UpdateUser:
+ * @UpdateDate: 2021/5/27 16:02
+ * @UpdateRemark: 更新说明
+ * @Version: 1.0
+ */
+class ImageUtil {
+}
---
+++ java/app/src/main/java/com/nowy/sharepic/ShareView.java
@@ -0,0 +1,14 @@
+package com.nowy.sharepic;
+
+/**
+ * @ClassName: ShareView
+ * @Description: 作用描述
+ * @Author: Nowy
+ * @CreateDate: 2021/5/27 15:38
+ * @UpdateUser:
+ * @UpdateDate: 2021/5/27 15:38
+ * @UpdateRemark: 更新说明
+ * @Version: 1.0
+ */
+class ShareView {
+}
